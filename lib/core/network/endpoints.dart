526d5397d074a9ef7b4e3e378f89051214742df3
engnouran12 <nourankadri@gmail.com>
2026-04-07 13:48:34 +0200

    feat: add app constants and network endpoints, update dependencies, and ignore sensitive configuration files

--- lib/core/network/endpoints.dart
+++
@@ -1,9 +0,0 @@
-class Endpoints {
-  static const String login = 'users/login';
-  static const String userInfo = 'users/info';
-  static const String register = 'users/register';
-  static const String verifyEmail = 'users/verifyEmail';
-  static const String categoriesList = 'categories';
-  static const String categoryDetails = 'categories/';
-  static const String coursesDetails = 'courses/';
-}
